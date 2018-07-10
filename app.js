b498e2d61440e3260d28f3c6f137371c795b0134
mfaibrahem <mfaibrahem2013@gmail.com>
2018-07-10 13:35:09 +0200

    commit to branch1

---
+++ app.js
@@ -0,0 +1,25 @@
+class Person {
+  constructor(fName, lName, dob) {
+    this.firstName = fName;
+    this.lastName = lName;
+    this.birthday = new Date(dob);
+  }
+  greeting() {
+    return `hello there ${this.firstName} ${this.lastName}`;
+  }
+  calculateAge() {
+    const diff = new Date().getTime() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+}
+
+const UImfaP = document.querySelector('.mfa-object');
+// instantiate an object
+const mfa = new Person('mostafa', 'ibrahem', '12-6-1993');
+
+UImfaP.innerHTML = `
+                    <span> note that <br> functions in the class are put in the prototype automatickally </span>
+                    <p>object greeting function ========> ${mfa.greeting()}</p>
+                    <p>object calculate age function ========> ${mfa.calculateAge()}</P>
+                  `;
