469e0d2f18b4ea18791816cd91273e5886e340ac
mfaibrahem <mfaibrahem2013@gmail.com>
2018-07-10 14:16:21 +0200

    add gets married in branch1

--- app.js
+++ app.js
@@ -12,14 +12,23 @@ class Person {
     const ageDate = new Date(diff);
     return Math.abs(ageDate.getUTCFullYear() - 1970);
   }
+  getsMarried(newLastName) {
+    this.lastName = newLastName;
+  }
 }
 
 const UImfaP = document.querySelector('.mfa-object');
 // instantiate an object
 const mfa = new Person('mostafa', 'ibrahem', '12-6-1993');
+const xWoman = new Person('xWoman', 'xMan', '12-10-80');
+
 
 UImfaP.innerHTML = `
                     <span> note that <br> functions in the class are put in the prototype automatickally </span>
                     <p>object greeting function ========> ${mfa.greeting()}</p>
                     <p>object calculate age function ========> ${mfa.calculateAge()}</P>
+                    <p>object new x woman greeting =====> ${xWoman.greeting()}</p>
                   `;
+
+xWoman.getsMarried('yMan');
+document.write(`after married y man ====> ${xWoman.greeting()}`);
